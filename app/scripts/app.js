3cb9a7986262d2b097a1d38fdde5a080dd2c57c5
Ryan Bennick <ryan@320ny.com>
2013-11-23 13:28:36 -0500

    Created check on user before asking for auth token

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -61,7 +61,11 @@ angular.module('timelineApp', [
     $httpProvider.responseInterceptors.push(interceptor);
   })
     .run(function ($rootScope, $location, $cookieStore, $http, Auth) {
-      var auth_token = $cookieStore.get('user').authentication_token || "";
+      var user = $cookieStore.get('user');
+      var auth_token = "";
+      if (user) {
+        auth_token = user.authentication_token || "";
+      }
 
       if (auth_token != ""){
         $http.defaults.headers.common["Authorization"]="Token token="+auth_token;
