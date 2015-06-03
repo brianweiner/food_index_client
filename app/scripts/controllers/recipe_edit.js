275e38cb8b847fcf4b854532aa2558776d1651bf
Brian Weiner <brian@320ny.com>
2015-06-03 08:32:57 -0400

    fixed update

--- app/scripts/controllers/recipe_edit.js
+++ app/scripts/controllers/recipe_edit.js
@@ -8,46 +8,43 @@ angular.module('timelineApp')
       $scope.user = Auth.user;
       $scope.recipe = Recipe.get({id: $stateParams.recipeId}, function(data){
         $scope.recipe = data.recipe;
+        $scope.local_recipe = {};
+        $scope.local_recipe.name = $scope.recipe.name;
+        $scope.local_recipe.description = $scope.recipe.description;
       });
+
       $scope.ingredient = {};
       $scope.step = {};
-    }
-
-    $scope.edit_title_and_description = function(){
-      $scope.show.edit_title_or_description = true;
-    }
-
-    $scope.add_ingredient = function(){
-      $scope.show.add_ingredient = true;
-    }
-
-    $scope.add_step = function(){
-      $scope.show.add_step = true;
-    }
-
-    $scope.update_recipe = function(){
-      Recipe.save({recipe: $scope.recipe, id: $scope.recipe.id}, function(data){
-        $scope.show.edit_title_or_description = false;
-      })
     }
 
     $scope.cancel = function(area){
       $scope.show[area] = false;
     }
 
+    $scope.enable = function(area){
+      $scope.show[area] = true;
+    }
+
+    $scope.update_recipe = function(){
+      Recipe.save({ recipe: $scope.local_recipe, id: $scope.recipe['id'] }, function(data){
+        angular.extend($scope.recipe, data.recipe);
+        $scope.cancel('edit_title_or_description');
+      });
+    }
+
     $scope.submit_recipe_ingredient = function(){
       Recipe.add_recipe_ingredient({recipe_ingredient: $scope.ingredient, id: $scope.recipe.id}, function(data){
         $scope.recipe.recipe_ingredients = $scope.recipe.recipe_ingredients.concat(data);
+        $scope.ingredient = {};
+        $scope.cancel('add_ingredient');
       });
-      $scope.show.add_ingredient = false;
-      $scope.ingredient = {};
     }
 
     $scope.submit_recipe_step = function(){
       Recipe.add_recipe_step({recipe_step: $scope.step, id: $scope.recipe.id}, function(data){
         $scope.recipe.recipe_steps = $scope.recipe.recipe_steps.concat(data);
+        $scope.step = {};
+        $scope.cancel('add_step');
       });
-      $scope.step = {};
-      $scope.show.add_step = false;
     }
   });
